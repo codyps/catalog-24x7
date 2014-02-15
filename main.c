0ebb908432613d5c03b68615e2e197ed8a93e0df
Cody P Schafer <devel@codyps.com>
2014-02-14 22:17:22 -0800

    things

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
 #define __packed __attribute__((__packed__))
-#include "hv-24x7.h"
+#include "hv-24x7-catalog.h"
+
+
+/* 2 mappings:
+ * - # to name
+ * - name to #
+ */
+enum hv_perf_domains {
+#define DOMAIN(n, v) HV_PERF_DOMAIN_##n = v,
+#include "hv-24x7-domains.h"
+#undef DOMAIN
+};
+
+static size_t domain_to_string(enum hv_perf_domains domain, char *buf, size_t buf_len)
+{
+	size_t l;
+	switch (domain) {
+#define DOMAIN(n, v)				\
+	case HV_PERF_DOMAIN_##n:		\
+		l = max(strlen(#n), buf_len);	\
+		memcpy(buf, #n, l);		\
+		break;
+#include "hv-24x7-domains.h"
+#undef DOMAIN
+		default:
+			l = snprintf(buf, buf_len, "unknown[%d]", domain);
+	}
+
+	return l;
+}
 
 int main(int argc, char **argv)
 {
