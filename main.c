0b7637043e671f23fba35259d916834a2d1c073e
Cody P Schafer <cody@linux.vnet.ibm.com>
2014-02-25 15:37:26 -0800

    licensing

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/*
+ * Author: Cody P Schafer <cody@linux.vnet.ibm.com>
+ * Copyright 2014 IBM Corporation.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
